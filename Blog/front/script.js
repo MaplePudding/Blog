a33b4e5432156639107835988c68d5ef1d7108d4
MaplePudding <fengtangbuding@gmail.com>
2020-06-11 17:42:26 +0800

    use Vue to build the project

--- Blog/front/script.js
+++ Blog/front/script.js
@@ -1,19 +1,90 @@
-window.onload = function(){
-    var button = this.document.querySelector('#button_menu');
-    var aside = this.document.querySelector('#aside')
-    button.addEventListener('click', function(){
-        if(aside.classList.length == 0){
-            aside.classList.add('extend');
-        }
-        else{
-            if(aside.classList.contains('extend')){
-                aside.classList.remove('extend');
-                aside.classList.add('shrink');
-            }
-            else{
-                aside.classList.remove('shrink');
-                aside.classList.add('extend');
-            }
-        }
-    })
+
+
+var homeTemplate = {
+    template: '<div class="blogContent"></div>'
 }
+
+Vue.component('home', homeTemplate);    // home element
+
+var searchTemplate = {
+    data(){
+        return{
+            broder: false,
+
+        }
+    },
+
+    methods:{
+        changeBroder: function(){
+            this.broder = true;
+        }
+    },
+
+    template: '<div id="searchContent" @mouseenter="changeBroder" v-bind:class="{bigger: broder, smaller: !broder}"><div><button><img src="image/search.png"></button><input type="text"/></div></div>'
+}
+
+Vue.component('search', searchTemplate);    // search element
+
+
+var categoryTemplate = {
+    template: '<div id="categoryContent></div>"'
+}
+
+Vue.component('category', categoryTemplate);    // category element
+/**
+ * create a router ovject
+ */
+
+var router = new VueRouter({
+    routes: [
+        {path: '/search' ,component: searchTemplate},
+        {path: '/category' ,component: categoryTemplate},
+        {path: '/home', component: homeTemplate}
+    ]
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        asideFlag: false,
+        show: false,
+        broder: false,
+        blogArray: [
+
+        ],
+    },
+
+    methods: {
+
+        /**
+         *  switching component
+         */
+
+        change: function () {
+            this.show = true;
+            this.asideFlag == false ? (this.asideFlag = true) : (this.asideFlag = false);
+        },
+
+        /**
+         * Back to top
+         */
+
+        toTop: function(){
+            scroll(0, 0);
+        },
+
+        smaller: function(){
+            this.broder = false;
+        } 
+    },
+
+    created: function () {
+
+    },
+
+    mounted: function () {
+
+    },
+
+    router: router,
+})
